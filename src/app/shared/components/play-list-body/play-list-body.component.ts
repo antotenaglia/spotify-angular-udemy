7b193733ef576302785ad2174dfc7dfa919c0df3
Antonela Tenaglia <antonelatenaglia@gmail.com>
2024-05-10 16:17:50 -0300

    order list asc/desc

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -9,9 +9,19 @@ import * as dataRaw from '../../../data/tracks.json';
 })
 export class PlayListBodyComponent implements OnInit {
   @Input() tracks: TrackModel[] = []
+  optionSort: { property: string | null, order: string } = { property: null, order: 'asc' }
 
   ngOnInit(): void {
     const { data }: any = (dataRaw as any).default
     this.tracks = data
+  }
+
+  changeSort(property: string): void {
+    const { order } = this.optionSort
+    this.optionSort = {
+      property,
+      order: order === 'asc' ? 'desc' : 'asc'
+    }
+    console.log(this.optionSort);
   }
 }
